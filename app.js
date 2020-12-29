41ddd424db453e58b253511da587c0ee559b9a69
Gigi <gigi@Eimans-MacBook-Pro.local>
2020-12-28 21:33:35 -0500

    API Connect

--- app.js
+++ app.js
@@ -1,97 +1,78 @@
-const URL = "https://covid-api.mmediagroup.fr/v1//cases?country=US"
+const URL = "https://covid-api.mmediagroup.fr/v1//cases?country=US";
 //const optionValue = document.querySelector("option").value
 // console.log(optionValue)
-let axiosData = []
-let div = document.querySelector(".searchbar")
+let axiosData = [];
+let div = document.querySelector(".searchbar");
 
 const getData = async () => {
   try {
     const headers = {
       headers: {
-        'Access-Control-Allow-Origin': '*',
+        "Access-Control-Allow-Origin": "*",
       },
-    }
-    const response = await axios.get(URL, headers)
-    axiosData = response.data
+    };
+    const response = await axios.get(URL, headers);
+    axiosData = response.data;
 
     //console.log(Object.keys(axiosData))
-    const arrStates = Object.keys(axiosData).sort()
-    console.log(response)
-    renderStates(arrStates)
-   //renderData(optionValue , axiosData)
-    
-   //removeStats()
+    const arrStates = Object.keys(axiosData).sort();
+    console.log(response);
+    console.log(arrStates);
+    renderStates(arrStates);
+    //renderData(optionValue , axiosData)
 
-  
-  
+    //removeStats()
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-  
-  
-  
-  
-}
+};
 
-getData()
+getData();
 const value = (e) => {
- e.preventDefault()
-const optionValue = document.querySelector("option").value
-renderData(optionValue, axiosData)
-}
+  e.preventDefault();
+  let stateOptions = document.querySelector("select");
+  //const optionValue = document.querySelector("option")
+  let stateSelected = stateOptions.options[stateOptions.selectedIndex].text;
+  console.log(stateSelected);
+  renderData(stateSelected);
+};
 
 const renderStates = (states) => {
-  let stateOptions = document.querySelector("select")
+  let stateOptions = document.querySelector("select");
   //console.log(stateOptions)
-  
-  states.forEach((state) => {
-    const newOption = document.createElement("option")
-    newOption.innerText = `${state}`
-    newOption.value = `${state}`
-    //console.log(newOption.value)
-    stateOptions.appendChild(newOption)
-   });
-}
 
-// function getValue(e) {
-//   e.preventDefault()
-//   console.log(optionValue)
-//   getData(optionValue)
-// }
+  states.forEach((state) => {
+    const newOption = document.createElement("option");
+    newOption.innerText = `${state}`;
+    newOption.value = `${state}`;
+    //console.log(newOption.value)
+    stateOptions.appendChild(newOption);
+  });
+};
+
 
 function removeStats() {
-  const removeOption = document.querySelector("form")
+  const removeOption = document.querySelector("form");
   while (removeOption.lastChild) {
-    console.log(removeOption.lastChild)
-    removeOption.removeChild(removeOption.lastChild)
-    
+    console.log(removeOption.lastChild);
+    removeOption.removeChild(removeOption.lastChild);
   }
-  
 }
 
-const renderData = () => {
+const renderData = (stateValue) => {
+  // console.log(stateValue)
+  // console.log(axiosData[stateValue])
+  let state = axiosData[stateValue];
+  let newInfoSection = document.querySelector("#newInfo");
 
-  let viewData = axiosData.map((state) => {
-    return state //=== optionValue
-  }) 
-  div.innerHTML += `
-<p>${viewData.confirmed}</p>
-<p>${viewData.deaths}</p>
-<p>${viewData.recovered}</p>
- `
-  
-  console.log(viewData)
-  
-}
+  newInfoSection.innerHTML += `
+<p>${state.confirmed}</p>
+<p>${state.deaths}</p>
+<p>${state.recovered}</p>
+ `;
 
+  //   console.log(viewData)
+};
 
-const form = document.querySelector("form")
-form.addEventListener("submit",value)
-//   (e) => {
-//   e.preventDefault()
-//   console.log(e.target.value)
-// }
-
-
-// )
-
+const form = document.querySelector("form");
+form.addEventListener("submit", value);
